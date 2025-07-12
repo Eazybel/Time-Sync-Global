f091ae630b69c249bdc95d4080b1e1f036817ca9
Eazybel <eazybel27@gmail.com>
2025-07-12 22:11:50 +0300

    `Added loading counter and optimized API calls with setTimeout`

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const baseCountry = document.getElementById('base-country'),
       main=document.getElementById("main-wrapper"),
       loading=document.getElementById("loading"),
       body=document.getElementById("body"),
-      btn=document.getElementById("convert-btn")
+      btn=document.getElementById("convert-btn");
+const counter=document.getElementById("counter")
       
+
 const shower=()=>{
         main.style.display="none"
         loading.style.display="block"
@@ -31,13 +33,24 @@ const hider=()=>{
               continue;
           }
               baseCountry.innerHTML+=`<option>${data.data[i].name}</option>`
-              compare1.innerHTML+=`<option>${data.data[i].name}</option>`
-              compare2.innerHTML+=`<option>${data.data[i].name}</option>`
               compare3.innerHTML+=`<option>${data.data[i].name}</option>`
+              compare2.innerHTML+=`<option>${data.data[i].name}</option>`
+              compare1.innerHTML+=`<option>${data.data[i].name}</option>`
           }
           
       })
  btn.addEventListener("click",(()=>{
+     let counters=5
+   shower()
+    setInterval(() => {
+            if(counters!=0){
+                  counter.innerText=counters
+                  counters--
+            }
+           
+          
+        }, 1000);
+         counter.innerText=""
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${baseCountry.value}&appid=d99bff6286cc266251816e686344a5f3`).then(
         res=>res.json()
     ).then(data=>{
@@ -53,4 +66,55 @@ const hider=()=>{
     ).catch(err=>{
         alert("Please make sure you typed correctly")
     })
+   
+    setTimeout(() => {
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${compare1.value}&appid=d99bff6286cc266251816e686344a5f3`).then(
+        res=>res.json()
+    ).then(data=>{
+        console.log(data)
+           fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=9SCC73ZVOL18&format=json&by=position&lat=${data[0].lat}&lng=${data[0].lon}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        compareTime1.innerText=data.formatted
+    })
+    }
+
+    )
+    }, 2000);
+    setTimeout(() => {
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${compare2.value}&appid=d99bff6286cc266251816e686344a5f3`).then(
+        res=>res.json()
+    ).then(data=>{
+        console.log(data)
+           fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=9SCC73ZVOL18&format=json&by=position&lat=${data[0].lat}&lng=${data[0].lon}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        compareTime2.innerText=data.formatted
+    })
+    }
+
+    )
+    }, 3000);
+    setTimeout(() => {
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${compare3.value}&appid=d99bff6286cc266251816e686344a5f3`).then(
+        res=>res.json()
+    ).then(data=>{
+        console.log(data)
+           fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=9SCC73ZVOL18&format=json&by=position&lat=${data[0].lat}&lng=${data[0].lon}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        compareTime3.innerText=data.formatted
+    })
+    }
+
+    )
+    }, 4000);
+    setTimeout(() => {
+       
+        hider()
+    }, 5000);
+   
  }))
